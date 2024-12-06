0a16dd1c6b7e2391cce843763cfeb5be92f6a5ef
danielcaverocavero <74834672+danielcaverocavero@users.noreply.github.com>
2024-12-06 18:07:59 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init(params) {
+    // Get the button
+let mybutton = document.getElementById("myBtn");
+mybutton.addEventListener('click', topFunction);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}    
+};
